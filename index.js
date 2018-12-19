40cc1f5c216749aca7127278dc4778e3ce7663c7
nauchikus <nauchikus@yandex.ru>
2018-12-19 21:53:14 +0300

    build(product): disable required modules
    disabled required modules for test travis-ci

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const MdToHtmlAction = require( './book-builder/actions/md-to-html-action' );
-const CreateBookContentsInfoAction = require( './book-builder/actions/create-book-contents-info-action' );
+// const MdToHtmlAction = require( './book-builder/actions/md-to-html-action' );
+// const CreateBookContentsInfoAction = require( './book-builder/actions/create-book-contents-info-action' );
 
 const TestModule = require( './test-module' );
 
 const build = async () => {
     TestModule.func();
-    await MdToHtmlAction.action();
-    await CreateBookContentsInfoAction.action();
+    // await MdToHtmlAction.action();
+    // await CreateBookContentsInfoAction.action();
 };
 
 
