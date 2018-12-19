85fb1f22077d8df89b34d14cd4066f079b0d2ffd
nauchikus <nauchikus@yandex.ru>
2018-12-19 20:56:58 +0300

    build(product): add index.js
    add index.js and change attr main in packge.json from ./bin/book-builder to ./index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const MdToHtmlAction = require( './book-builder/actions/md-to-html-action' );
+const CreateBookContentsInfoAction = require( './book-builder/actions/create-book-contents-info-action' );
+
+const build = async () => {
+    await MdToHtmlAction.action();
+    await CreateBookContentsInfoAction.action();
+};
+
+
+module.exports = { build };
