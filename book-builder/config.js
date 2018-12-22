5cbc54c5902be1060f3033683b9c861de4a21d55
nauchikus <nauchikus@yandex.ru>
2018-12-22 10:49:21 +0300

    fix(components): fix import env-utils module
    fix import env-utils module in config.js

--- book-builder/config.js
+++ book-builder/config.js
@@ -1,7 +1,7 @@
-const EnvUtils = require( '../utils/env-utils' );
+const EnvUtils = require( './utils/env-utils' );
 
 const NODE_ENV = process.env.NODE_ENV;
-const PREFIX_PATH = NODE_ENV === 'production' ? EnvUtils.getProp( 'prod_assets_link_prefix' ) : '/';
+const PREFIX_PATH = NODE_ENV === 'production' ? EnvUtils.getProp( 'prod_assets_link_prefix' ) : '';
 
 module.exports = {
     PREFIX_PATH
